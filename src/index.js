855487fd1bc0732abc61434a1b5e2fb9e9800243
Ashik Khan <aasshhik98@gmail.com>
2022-12-14 02:24:06 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
+import initScrollReveal from "./src/scripts/scrollReveal";
+import initTiltEffect from "./src/scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
